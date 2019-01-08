88a5393838df99d28493742d0fadb5e9cc52e1d7
Charles Gunn <cjgunnmail@gmail.com>
2019-01-07 21:43:54 -0500

    full skeleton

--- js/robot.js
+++ js/robot.js
@@ -1,26 +1,63 @@
 import {BCAbstractRobot, SPECS} from 'battlecode';
-/*import runCrusader from './crusader';
+/*
+import runCrusader from './crusader';
 import runChurch from './church';
 import runCastle from './castle';
 import runPilgrim from './pilgrim';
 import runPreacher from './preacher';
 import runProphet from './prophet';
 */
-export default class MyRobot extends BCAbstractRobot {
-    turn() {
-      /*
-      switch (this.me.unit) {
-        case SPECS['castle']: return runCastle(this);
-        case SPECS['crusader']: return runCrusader(this);
-        case SPECS['church']: return runChurch(this);
-        case SPECS['pilgrim']: return runPilgrim(this);
-        case SPECS['preacher']: return runPreacher(this);
-        case SPECS['prophet']: return runProphet(this);
-      }*/
-      if(this.me.unit > 1) {
-        this.move(0,1);
-      }
+
+class MyRobot extends BCAbstractRobot {
+  turn() {
+    switch (this.me.unit) {
+      case SPECS.CASTLE:
+        return runCastle(this);
+      case SPECS.CRUSADER:
+        return runCrusader(this);
+      case SPECS.CHURCH:
+        return runChurch(this);
+      case SPECS.PILGRIM:
+        return runPilgrim(this);
+      case SPECS.PREACHER:
+        return runPreacher(this);
+      case SPECS.PROPHET:
+        return runProphet(this);
     }
+  }
 }
 
 var robot = new MyRobot();
+
+// castle.js
+function runCastle(m){
+  return m.buildUnit(SPECS.PILGRIM, 1, 1);
+}
+
+// church.js
+function runChurch(m){
+  return;
+}
+
+// pilgrim.js
+function runPilgrim(m){
+  const choices = [[0,-1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
+  const choice = choices[Math.floor(Math.random()*choices.length)]
+  m.log("Moved: (" + choice[0] + ", " + choice[1] + ")")
+  return m.move(...choice);
+}
+
+// crusader.js
+function runCrusader(m){
+  return m.move(0,1);
+}
+
+// preacher.js
+function runPreacher(m){
+  return m.move(0,1);
+}
+
+// prophet.js
+function runProphet(m){
+  return m.move(0,1);
+}
