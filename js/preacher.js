04d7ea66ca3c05eabd4bbb7f47bd6bb455f21733
wish2fun <35409301+wish2fun@users.noreply.github.com>
2019-01-09 00:06:26 -0500

    Updated Preacher

--- js/preacher.js
+++ js/preacher.js
@@ -1,3 +1,38 @@
+import { Pathfinder, exact_pred } from './pathfinder.js';
+import { calcOpposite, idx } from './helpers.js';
+
 export function runPreacher(m) {
-    return m.move(0, 1);
+  m.log("PREACHER ID: " + m.me.id + "  X: " + m.me.x + "  Y: " + m.me.y);
+  if (typeof m.pathfinder === "undefined") {
+      let opp = calcOpposite(m, m.spawn_castle.x, m.spawn_castle.y);
+      opp[0]--;
+      m.pathfinder = new Pathfinder(m, exact_pred(...opp));
+  }
+  let next = m.pathfinder.next_loc(m);
+  if (next === undefined && shouldAttack(m) && m.me.fuel >= m.stats.get("ac")) {
+    return m.attack(1, 0);
+  }
+  else if (next === -1) {
+      m.log("PREACHER STUCK");
+  }
+  else {
+      m.log("PREACHER MOVING: " + next);
+      let dx = next[0] - m.me.x; let dy = next[1] - m.me.y;
+      return m.move(dx, dy);
+  }
+}
+
+function shouldAttack(m) {
+    for (i = -1; i <= 1; i++) {
+      for (j = -1; j <= 1; j++) {
+        if (i != 0 || j != 0) {
+          if (idx(m.visible_map,m.me.x + i, m.me.y+j) != 0 && idx(m.visible_map,m.me.x + i, m.me.y+j) != 1) {
+            if (m.getRobot(idx(m.visible_map,m.me.x + i, m.me.y+j)).me.team == this.me.team) {
+              return false;
+            }
+          }
+        }
+      }
+    }
+    return true;
 }
