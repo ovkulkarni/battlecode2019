4ae7fa77e45892bb39da5de90ec0cdd33998d65d
ovkulkarni <2019okulkarn@tjhsst.edu>
2019-01-08 20:16:33 -0500

    fix recalculation

--- js/pathfinder.js
+++ js/pathfinder.js
@@ -9,8 +9,8 @@ export function karbonite_pred(m) {
     return ((x, y) => idx(m.karbonite_map, x, y));
 }
 export function on_path(path) {
-  let spath = path.map(a => a.toString());
-  return ((x, y) => path.includes([x, y].toString()));
+    let spath = path.map(a => a.toString());
+    return ((x, y) => path.includes([x, y].toString()));
 }
 
 export class Pathfinder {
@@ -21,10 +21,11 @@ export class Pathfinder {
         this.speed = SPECS.UNITS[m.me.unit].SPEED;
         this.recalculate();
     }
-    next_loc(m, wait=false) {
+    next_loc(m, wait = false) {
         let next = this.path[0];
-        if(next === undefined) return undefined;
-        if (idx(m.getVisibleRobotMap(), ...next) === 1) {
+        if (next === undefined) return undefined;
+        occupied = idx(m.visible_map, ...next);
+        if (idx !== 0 && idx !== -1) {
             // add ability to go around better
             if (wait) {
                 m.log("WAITING");
@@ -32,20 +33,19 @@ export class Pathfinder {
             }
             else {
                 m.log("RECALCULATING");
-                return -1;
                 this.recalculate();
                 m.log("RECALCULATING DONE");
             }
         }
         let result = this.path.shift();
-        if(result[0] === this.loc[0] && result[1] === this.loc[1]) {
+        if (result[0] === this.loc[0] && result[1] === this.loc[1]) {
             result = this.path.shift();
         }
         this.loc = result
         m.log("NEXT MOVE: " + result);
         return result;
     }
-    recalculate(){
+    recalculate() {
         this.path = this.find_path(this.goal);
     }
     find_path(pred) {
@@ -79,64 +79,64 @@ export class Pathfinder {
 }
 
 function LinkedList() {
-  this.head = null;
-  this.tail = null;
+    this.head = null;
+    this.tail = null;
 }
 
 function Node(value, next, prev) {
-  this.value = value;
-  this.next = next;
-  this.prev = prev;
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
 }
 
 // Add nodes methods
 
 LinkedList.prototype.addToHead = function (value) {
-  const newNode = new Node(value, this.head, null);
-  if (this.head) this.head.prev = newNode;
-  else this.tail = newNode;
-  this.head = newNode;
+    const newNode = new Node(value, this.head, null);
+    if (this.head) this.head.prev = newNode;
+    else this.tail = newNode;
+    this.head = newNode;
 };
 
 LinkedList.prototype.addToTail = function (value) {
-  const newNode = new Node(value, null, this.tail);
-  if (this.tail) this.tail.next = newNode;
-  else this.head = newNode;
-  this.tail = newNode;
+    const newNode = new Node(value, null, this.tail);
+    if (this.tail) this.tail.next = newNode;
+    else this.head = newNode;
+    this.tail = newNode;
 }
 
 // Remove nodes methods
 LinkedList.prototype.removeHead = function () {
-  if (!this.head) return null;
-  let value = this.head.value;
-  this.head = this.head.next;
+    if (!this.head) return null;
+    let value = this.head.value;
+    this.head = this.head.next;
 
-  if (this.head) this.head.prev = null;
-  else this.tail = null;
+    if (this.head) this.head.prev = null;
+    else this.tail = null;
 
-  return value;
+    return value;
 
 }
 
 LinkedList.prototype.removeTail = function () {
-  if (!this.tail) return null;
-  let value = this.tail.value;
-  this.tail = this.tail.prev;
+    if (!this.tail) return null;
+    let value = this.tail.value;
+    this.tail = this.tail.prev;
 
-  if (this.tail) this.tail.next = null;
-  else this.head = null;
+    if (this.tail) this.tail.next = null;
+    else this.head = null;
 
-  return value;
+    return value;
 }
 
 // Search method
 
 LinkedList.prototype.search = function (searchValue) {
-  let currentNode = this.head;
+    let currentNode = this.head;
 
-  while (currentNode) {
-    if (currentNode.value === searchValue) return currentNode;
-    currentNode = currentNode.next;
-  }
-  return null;
+    while (currentNode) {
+        if (currentNode.value === searchValue) return currentNode;
+        currentNode = currentNode.next;
+    }
+    return null;
 }
