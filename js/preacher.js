187864934cdb1c5cdfccb6d4c4b896838734aa1e
ovkulkarni <2019okulkarn@tjhsst.edu>
2019-01-09 21:52:34 -0500

    use .fin

--- js/preacher.js
+++ js/preacher.js
@@ -11,7 +11,7 @@ export function runPreacher(m) {
         m.pathfinder = new Pathfinder(m, attack_pred(m, m.spawn_castle.x, m.spawn_castle.y));
     }
     let next = m.pathfinder.next_loc(m);
-    if (next === undefined) {
+    if (next.fin) {
         m.log("FUEL " + m.fuel + " " + m.stats.FUEL_CAPACITY);
         if (shouldAttack(m) && (m.fuel >= m.stats.FUEL_CAPACITY)) {
             m.log("PREACHER ATTACKED");
@@ -35,12 +35,12 @@ export function shouldAttack(m) {
     for (let i = -1; i <= 1; i++) {
         for (let j = -1; j <= 1; j++) {
             if (i != 0 || j != 0) {
-                let id = idx(m.getVisibleRobotMap(), m.me.x + i+1, m.me.y + j);
+                let id = idx(m.getVisibleRobotMap(), m.me.x + i + 1, m.me.y + j);
                 m.log("ROBOT ID " + id);
                 if (id != 0 && id != -1) {
                     m.log("TEAM " + m.getRobot(id).team);
                     //if (m.getRobot(id).team === m.team) {
-                        return true;
+                    return true;
                     //}
                 }
             }
