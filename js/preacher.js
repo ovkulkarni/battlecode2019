13c3c298e31ef545f8fdfead30b2a07d9de87de3
wish2fun <35409301+wish2fun@users.noreply.github.com>
2019-01-09 21:36:26 -0500

    added attack_pred to preacher

--- js/preacher.js
+++ js/preacher.js
@@ -8,26 +8,26 @@ export function runPreacher(m) {
         m.stats = get_stats(m);
     }
     if (typeof m.pathfinder === "undefined") {
-        let opp = calcOpposite(m, m.spawn_castle.x, m.spawn_castle.y);
-        opp[0]--;
-        m.pathfinder = new Pathfinder(m, exact_pred(...opp));
+        m.pathfinder = new Pathfinder(m, attack_pred(m, m.spawn_castle.x, m.spawn_castle.y));
     }
     let next = m.pathfinder.next_loc(m);
-    if (next === undefined) {
+    if (next.fin) {
         m.log("FUEL " + m.fuel + " " + m.stats.get("fc"));
         if (shouldAttack(m) && (m.fuel >= m.stats.get("fc"))) {
             m.log("PREACHER ATTACKED");
             return m.attack(1, 0);
         }
+        return;
     }
-    else if (next === -1) {
+    else if (next.wait || next.fail) {
         m.log("PREACHER STUCK");
+        return;
     }
     else {
         m.log("PREACHER MOVING: " + next);
-        let dx = next[0] - m.me.x; let dy = next[1] - m.me.y;
-        return m.move(dx, dy);
-    }   
+        //let dx = next[0] - m.me.x; let dy = next[1] - m.me.y;
+        return m.move(...next.diff);
+    }
 }
 
 export function shouldAttack(m) {
@@ -35,16 +35,16 @@ export function shouldAttack(m) {
     for (let i = -1; i <= 1; i++) {
         for (let j = -1; j <= 1; j++) {
             if (i != 0 || j != 0) {
-                let id = idx(m.getVisibleRobotMap(), m.me.x + i, m.me.y + j);
+                let id = idx(m.getVisibleRobotMap(), m.me.x + i+1, m.me.y + j);
                 m.log("ROBOT ID " + id);
                 if (id != 0 && id != -1) {
                     m.log("TEAM " + m.getRobot(id).team);
-                    if (m.getRobot(id).team === m.team) {
+                    //if (m.getRobot(id).team === m.team) {
                         return true;
-                    }
+                    //}
                 }
             }
         }
     }
-    return true;
+    return false;
 }
