b9e5d417d78f868fdd5ed4a2333ff54176068f18
ovkulkarni <2019okulkarn@tjhsst.edu>
2019-01-10 13:25:01 -0500

    fix

--- js/robot.js
+++ js/robot.js
@@ -8,13 +8,17 @@ import { runPreacher } from './preacher.js';
 import { runProphet } from './prophet.js';
 import { get_stats } from './helpers.js';
 
-
 class MyRobot extends BCAbstractRobot {
     turn() {
         this.visible_map = this.getVisibleRobotMap();
         this.visible_robots = this.getVisibleRobots();
-        this.visible_allies = this.visible_robots.filter(r => r.castle_talk !== undefined).filter(r => this.me.id !== r.id);
-        this.visible_enemies = this.visible_robots.filter(r => r.castle_talk === undefined);
+        if (this.me.unit === SPECS.CASTLE) {
+            this.visible_allies = this.visible_robots.filter(r => r.castle_talk !== undefined).filter(r => this.me.id !== r.id);
+            this.visible_enemies = this.visible_robots.filter(r => r.castle_talk === undefined);
+        } else {
+            this.visible_allies = this.visible_robots.filter(r => r.team === this.me.team).filter(r => r.id !== this.me.id);
+            this.visible_enemies = this.visible_robots.filter(r => r.team !== this.me.team);
+        }
         if (this.stats === undefined)
             this.stats = get_stats(this);
         if (this.symmetry === undefined)
