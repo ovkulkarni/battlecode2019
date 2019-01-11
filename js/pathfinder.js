7f53b9172d634c96a0f1c4f741693d243f816709
ovkulkarni <2019okulkarn@tjhsst.edu>
2019-01-10 22:11:44 -0500

    pathfinder is a class

--- js/pathfinder.js
+++ js/pathfinder.js
@@ -73,72 +73,67 @@ export class Pathfinder {
     }
 }
 
-function LinkedList() {
-    this.head = null;
-    this.tail = null;
-    this.len = 0;
-}
-
-function Node(value, next, prev) {
-    this.value = value;
-    this.next = next;
-    this.prev = prev;
-}
-
-// Add nodes methods
-
-LinkedList.prototype.addToHead = function (value) {
-    const newNode = new Node(value, this.head, null);
-    if (this.head) this.head.prev = newNode;
-    else this.tail = newNode;
-    this.head = newNode;
-    this.len += 1;
-};
-
-LinkedList.prototype.addToTail = function (value) {
-    const newNode = new Node(value, null, this.tail);
-    if (this.tail) this.tail.next = newNode;
-    else this.head = newNode;
-    this.tail = newNode;
-    this.len += 1;
-}
-
-// Remove nodes methods
-LinkedList.prototype.removeHead = function () {
-    if (!this.head) return null;
-    let value = this.head.value;
-    this.head = this.head.next;
-
-    if (this.head) this.head.prev = null;
-    else this.tail = null;
-    this.len -= 1;
-    return value;
-
-}
-
-LinkedList.prototype.removeTail = function () {
-    if (!this.tail) return null;
-    let value = this.tail.value;
-    this.tail = this.tail.prev;
-
-    if (this.tail) this.tail.next = null;
-    else this.head = null;
-    this.len -= 1;
-    return value;
-}
-
-// Search method
-
-LinkedList.prototype.search = function (searchValue) {
-    let currentNode = this.head;
-
-    while (currentNode) {
-        if (currentNode.value === searchValue) return currentNode;
-        currentNode = currentNode.next;
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.len = 0;
     }
-    return null;
+
+    addToHead(value) {
+        const newNode = new Node(value, this.head, null);
+        if (this.head) this.head.prev = newNode;
+        else this.tail = newNode;
+        this.head = newNode;
+        this.len += 1;
+    };
+
+    addToTail(value) {
+        const newNode = new Node(value, null, this.tail);
+        if (this.tail) this.tail.next = newNode;
+        else this.head = newNode;
+        this.tail = newNode;
+        this.len += 1;
+    }
+    removeHead() {
+        if (!this.head) return null;
+        let value = this.head.value;
+        this.head = this.head.next;
+
+        if (this.head) this.head.prev = null;
+        else this.tail = null;
+        this.len -= 1;
+        return value;
+    }
+
+    removeTail() {
+        if (!this.tail) return null;
+        let value = this.tail.value;
+        this.tail = this.tail.prev;
+
+        if (this.tail) this.tail.next = null;
+        else this.head = null;
+        this.len -= 1;
+        return value;
+    }
+    search(searchValue) {
+        let currentNode = this.head;
+
+        while (currentNode) {
+            if (currentNode.value === searchValue) return currentNode;
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+    len() {
+        return this.len;
+    }
 }
 
-LinkedList.prototype.len = function () {
-    return this.len;
+class Node {
+    constructor(value, next, prev) {
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
+    }
 }
