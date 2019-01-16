faffc129dc1f6151ad3d3ab74607b4dfd1bef6f3
wish2fun <35409301+wish2fun@users.noreply.github.com>
2019-01-15 19:29:47 -0500

    hording for preachers

--- js/preacher.js
+++ js/preacher.js
@@ -1,20 +1,38 @@
 import { Pathfinder } from './pathfinder.js';
-import { get_stats, calcOpposite, idx, dis } from './helpers.js';
-import { attack_pred } from "./predicates.js";
-import { constants } from "./constants.js"
-import { horde } from './analyzemap.js';
+import { attack_pred, around_pred } from './predicates.js';
+import { calcOpposite, dis, create_augmented_obj, get_mission, idx, passable_loc } from './helpers.js';
+import { constants } from './constants.js';
+import { wander } from './analyzemap.js';
+import { decode16 } from './communication.js';
 
 export function runPreacher(m) {
     m.log("PREACHER ID: " + m.me.id + "  X: " + m.me.x + "  Y: " + m.me.y);
     if (m.me.turn === 1) {
+        let opp = calcOpposite(m, m.spawn_castle.x, m.spawn_castle.y);;
         switch (m.mission) {
             case constants.ATTACK:
-                let opp = calcOpposite(m, m.spawn_castle.x, m.spawn_castle.y);
                 m.pathfinder = new Pathfinder(m, attack_pred(m, ...opp));
+                break;
+            case constants.HORDE:
+                m.horde_loc = { x: opp[0], y: opp[1] }
                 break;
             default:
                 m.pathfinder = new Pathfinder(m, attack_pred(m, m.spawn_castle.x, m.spawn_castle.y));
                 break;
+        }
+    }
+    for (let r of m.visible_allies) {
+        if (r.signal !== -1) {
+            let message = decode16(r.signal);
+            m.log(`GOT COMMAND ${message.command} (${message.args}) FROM ${r.id}`);
+            if (message.command === "send_horde") {
+                m.horde_loc = {};
+                m.horde_loc.x = message.args[0];
+                m.horde_loc.y = message.args[1];
+                m.begin_horde = true;
+            } else if (message.command === "task") {
+                m.mission = message.args[0];
+            }
         }
     }
     for (let r of m.visible_enemies) {
@@ -24,16 +42,47 @@ export function runPreacher(m) {
             return m.attack(r.x - m.me.x, r.y - m.me.y);
         }
     }
-    let h = horde(m);
-    if (h === false)
-        return;
+    if (m.mission === constants.HORDE) {
+        if (m.begin_horde) {
+            if (m.intermediate_point === undefined) {
+                m.log(`Trying to find path from ${JSON.stringify(m.spawn_castle)} to ${JSON.stringify(m.horde_loc)}`);
+                let pf = new Pathfinder(create_augmented_obj(m, m.spawn_castle.x, m.spawn_castle.y), attack_pred(m, m.horde_loc.x, m.horde_loc.y));
+                if (pf.path === undefined) {
+                    m.log(`NO PATH FROM CASTLE TO OPPOSITE :(`);
+                    return;
+                }
+                m.intermediate_point = pf.path[Math.floor(pf.path.length / 2)];
+                return;
+            } else {
+                m.pathfinder = new Pathfinder(m, around_pred(...m.intermediate_point, 1, 2));
+                m.begin_horde = false;
+                m.on_intermediate = true;
+                m.started = true;
+            }
+        } else if (!m.started) {
+            return;
+        }
+    }
     let next = m.pathfinder.next_loc(m);
+    if (next.fail) { m.log("FAILED"); return; }
+    if (next.wait) { m.log("WAITING"); return; }
     if (next.fin) {
         switch (m.mission) {
-            case constants.HORDE_INTERMEDIATE:
-                m.mission = constants.ATTACK;
-                let opp = calcOpposite(m, m.spawn_castle.x, m.spawn_castle.y);
-                m.pathfinder = new Pathfinder(m, attack_pred(m, ...opp));
+            case constants.HORDE:
+                if (m.on_intermediate) {
+                    m.on_intermediate = false;
+                    m.pathfinder = new Pathfinder(m, attack_pred(m, m.horde_loc.x, m.horde_loc.y));
+                    return;
+                } else {
+                    m.mission = constants.RETURN;
+                    m.pathfinder = new Pathfinder(m, around_pred(m.spawn_castle.x, m.spawn_castle.y, 1, 3));
+                    m.begin_horde = undefined;
+                    m.intermediate_point = undefined;
+                    m.on_intermediate = undefined;
+                    m.started = undefined;
+                    return;
+                }
+            case constants.RETURN:
                 return;
             default:
                 m.mission = constants.NEUTRAL;
@@ -42,15 +91,8 @@ export function runPreacher(m) {
                 return;
         }
     }
-    else if (next.wait || next.fail) {
-        m.log("PREACHER STUCK");
-        return;
-    }
-    else {
-        m.log("PREACHER MOVING: " + next);
-        //let dx = next[0] - m.me.x; let dy = next[1] - m.me.y;
-        return m.move(...next.diff);
-    }
+    return m.move(...next.diff);
+
 }
 
 export function shouldAttack(m, x, y) {
@@ -59,15 +101,17 @@ export function shouldAttack(m, x, y) {
     for (let i = -1; i <= 1; i++) {
         for (let j = -1; j <= 1; j++) {
             //if (i != 0 || j != 0) {
-            let id = idx(m.visible_map, m.me.x + i + x, m.me.y + j + y);
-            // m.log("ROBOT ID " + id);
-            if (id !== 0 && id !== -1) {
-                // m.log("TEAM " + m.getRobot(id).team);
-                if (m.getRobot(id).team === m.team) {
-                    count = count - 1;
-                }
-                else count = count + 1;
-            }
+            if (passable_loc(m, m.me.x + i + x, m.me.y+j+y)) {
+              let id = idx(m.visible_map, m.me.x + i + x, m.me.y + j + y);
+              // m.log("ROBOT ID " + id);
+              if (id !== 0 && id !== -1) {
+                  // m.log("TEAM " + m.getRobot(id).team);
+                  if (m.getRobot(id).team === m.team) {
+                      count = count - 1;
+                  }
+                  else count = count + 1;
+              }
+          }
             //}
         }
     }
