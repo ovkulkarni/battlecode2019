df4c451f0078e77dde02963ea016d00f44423b9d
ovkulkarni <2019okulkarn@tjhsst.edu>
2019-01-22 14:48:37 -0500

    do this

--- js/castle.js
+++ js/castle.js
@@ -144,7 +144,7 @@ function handle_horde(m) {
         m.log("SENDING HORDE OF SIZE: " + m.current_horde);
         m.signal(encode16("send_horde", ...best_e_loc, m.friendly_ids.indexOf(`${best_e_id}`)), 20 * 20);
         if (m.max_horde_size < m.ultimate_horde_size)
-            m.max_horde_size = Math.floor(m.me.turn / 30);
+            m.max_horde_size += 2
         m.current_horde = 0;
 
         event_complete(m);
